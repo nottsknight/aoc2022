1b2c4cd46772be4bd619d62a954bb9b7587f8f5b
Ian Knight <ian.knight2@nottingham.ac.uk>
2022-12-04 18:27:01 +0000

    Add parseString

--- Utils/Parse.hs
+++ Utils/Parse.hs
@@ -1,7 +1,8 @@
-module Utils.Parse (dropSpace, parseInt, parseChar) where
+module Utils.Parse (dropSpace, parseInt, parseChar, parseString) where
 
 import Control.Monad.State (StateT (StateT))
 import Data.Char (isDigit, isSpace)
+import Data.List (isPrefixOf)
 
 parseInt :: StateT String Maybe Int
 parseInt = StateT $ \cs ->
@@ -14,6 +15,13 @@ parseChar :: Char -> StateT String Maybe Char
 parseChar c = StateT $ \cs ->
   let (c' : cs') = dropSpace cs
    in if c == c' then Just (c, cs') else Nothing
+
+parseString :: String -> StateT String Maybe String
+parseString s = StateT $ \cs ->
+  let cs' = dropSpace cs
+   in if s `isPrefixOf` cs'
+        then Just (s, drop (length s) cs')
+        else Nothing
 
 dropSpace :: String -> String
 dropSpace = dropWhile isSpace
